fab4a0f38d5fcf9119007913ec906e89e477f987
tezard <tezard@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-06-12 09:27:49 +0000

    carry labels through when buildingApe
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/paleophylo@46 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/buildApe.R
+++ pkg/R/buildApe.R
@@ -1,14 +1,15 @@
-`buildApe` <- function(pP)
+`buildApe` <- function(pP, label=TRUE)
 	{	
 	if(class(pP)!="paleoPhylo") stop("object is not of class 'paleoPhylo'")
 		{
 		get.line<-function (pn,dat,stNode,enNode=stNode)
 			{
-			tmp <- data.frame(nm=pP$nm[-which(is.na(pP$pn))],pn=pP$pn[-which(is.na(pP$pn))],st=pP$st[-which(is.na(pP$pn))],en=pP$en[-which(is.na(pP$pn))],Speciation=pP$Speciation[-which(is.na(pP$pn))],cd=pP$cd[-which(is.na(pP$pn))])
-			tmp<-tmp[as.character(tmp$pn)==pn,]
+			tmp <- with(dat,data.frame(nm=nm,pn=pn,st=st,en=en,Speciation=Speciation,cd=cd, label=label))
+			tmp <- tmp[!is.na(tmp$pn),]
+			tmp <- tmp[as.character(tmp$pn)==pn,]
 	
 			if(length(tmp[,1])>0) {
-					vv<-data.frame(IDcode=tmp$pn, IDname=rep(dat$nm[as.character(dat$nm)==as.character(tmp$pn)[1]],2),descName=tmp$nm,descCode=tmp$cd,duration=tmp$st-tmp$en,
+					vv<-data.frame(IDcode=tmp$pn, IDname=rep(dat$label[dat$nm==tmp$pn[1]],2),descName=tmp$nm,descCode=tmp$cd,duration=tmp$st-tmp$en,
 					tip=0+(tmp$Speciation==0),extant= 0 + (tmp$en==min(dat$en)),startNode=rep(stNode,2),endNode=enNode+1:2,startTime=tmp$st,endTime=tmp$en)}
 			if(length(tmp[,1])==1) {vv <- vv[1,]}
 			vv<-vv[order(rev(vv$startTime)),]
@@ -38,16 +39,21 @@
 	
 		clade$endNode[clade$tip==1]<-clade$descCode[clade$tip==1]
 		edge<-matrix(as.numeric(as.factor(c(clade$startNode,clade$endNode))),ncol=2)
-		
 		clade$edge2 <- edge[,2]
-		tL <- data.frame(tL=as.character(clade$descName[clade$tip==1]),ed=clade$edge2[clade$tip==1])
+		
+		clade$descLab <- NA
+		for(i in 1:length(clade[,1])) {clade$descLab[i] <- (pP$label[pP$nm==clade$descName[i]])}
+		
+		ifelse(label==TRUE,tL <- data.frame(tL=as.character(clade$descLab[clade$tip==1]),ed=clade$edge2[clade$tip==1]), 				tL <- data.frame(tL=as.character(clade$descName[clade$tip==1]),ed=clade$edge2[clade$tip==1]))
 		tL <- as.character(tL[order(tL$ed),1])
+		ifelse(label==TRUE, nL <- c(pn,as.character(clade$descLab[clade$tip==0])), nL <- c(pn,as.character(clade$descName[clade$tip==0])))
 
 		clade$nodeLabel<-edge[,1]
-		if (sum(with(clade,tapply(nodeLabel,nodeLabel,length))!=2) !=0) {warning("bifuracting walk through has failed to yield a completely bifurcating tree")}
+		if (sum(with(clade,tapply(nodeLabel,nodeLabel,length))!=2) !=0)
+				{warning("bifuracting walk through has failed to yield a completely bifurcating tree")}
 	    
-		mft <- list(edge = edge, tip.label = tL, Nnode = length(unique(clade$IDcode)), node.label = c(pn,as.character(clade$descName[clade$tip==0])),
-				edge.length=clade$duration)
+		mft <- list(edge = edge, tip.label = tL, Nnode = length(unique(clade$IDcode)), 
+				  node.label = nL, edge.length=clade$duration)
 		class(mft) <- "phylo"	
 		return(mft)
 		}
