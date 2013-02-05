ba26aa0406a079dae8ecc56a38f7c7e7f45e6877
tezard <tezard@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-02-05 17:30:30 +0000

    better r2r to calculate proper durations
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/paleophylo@134 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/route2root.R
+++ pkg/R/route2root.R
@@ -7,19 +7,18 @@ route2root <- function(pP, focLin)
   rt <- pP$nm[x]
   
   allAnc <- immAnc <- focLin
-  dur <- pP$st - pP$en
-  tm2rt <- c()
+  tm2rt  <- aL$en[aL$nm==focLin]
   
   while(immAnc != rt)
     {
     i <- which(pP$nm == immAnc)
-    tm2rt <- c(tm2rt, dur[i])
+    tm2rt <- c(tm2rt, pP$st[i])
     immAnc <- pP$pn[i]
     allAnc <- c(allAnc,immAnc)
     }
-  tm2rt <- c(tm2rt, rtDur)
+    
   if(length(allAnc)!=length(tm2rt)) stop ("Number of lineages does not match number of durations.")
-  
+  tm2rt <- diff(c(tm2rt, pP$st[x]))
   out <- list(path=rev(allAnc), duration=rev(tm2rt), nNode=length(allAnc))
   return(out)
   }
