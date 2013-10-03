0f64079743d23c7eb37f628719390d55a293a65a
tezard <tezard@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-10-03 14:43:00 +0000

    correct rwb error
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/paleophylo@137 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/rwb.R
+++ pkg/R/rwb.R
@@ -1,37 +1,47 @@
-rwb <- function(pP, bL=1, st=max(pP$st), en=min(pP$en))
+rwb <- function (pP, bL = 1, st = max(pP$st), en = min(pP$en)) 
   {
-  if(class(pP)!="paleoPhylo") stop("Object is not of class paleoPhylo")
-  if (en >= st) stop("End must be later than start.")
-  if (bL <= 0) stop("Bin Length must be positive.")
+  if (class(pP) != "paleoPhylo") 
+       stop("Object is not of class paleoPhylo")
+  if (en >= st) 
+        stop("End must be later than start.")
+
+  bpp <- with(pP, data.frame(nm, pn, st, en))
+   
+  if(length(bL) > 1) {bins <- bL}
+  if(length(bL) == 1) {bins <- seq(st, en, -bL)}
+  nb <- length(bins)
+  if (nb <= 1) 
+        stop("Bin Length must be larger than 1.")
   #check there's an interval within which rates will be calculated
   
   # Lineages persisting to the specified end are not considered to have gone extinct.
   #Events taking place exactly on bin boundaries are taken to occur in the later bin.
 
-  pPcB <- createBifurcate(pP)
-  ppdf <- data.frame(nm=pPcB$nm, pn=pPcB$pn, st=pPcB$st, en=pPcB$en)
-
-  if(length(bL)==1) bins <- seq(st, en, -bL)
-  if(length(bL)>1) bins <- bL
-  nb <- length(bins)
-  rwb <- data.frame(binStart=bins, nStart=rep(NA, nb), branchLength=rep(NA, nb), orig=rep(NA, nb), extn=rep(NA, nb), lambda=rep(NA, nb), mu=rep(NA, nb))
- 
-  for(k in 1:(nb-1))
-    {
-    inBin <- ppdf[ppdf$st>bins[k+1] & ppdf$en<=bins[k],]
-    nn  <- sum(inBin$st>bins[k])
-    noDesc <- sapply(1:dim(inBin)[1], function(i) sum(as.character(inBin$pn)==inBin$nm[i], na.rm=TRUE))==0
-    noRoot <- !is.na(inBin$pn)
-    orig <- sum(inBin$st<=bins[k] & noRoot)/2
-    extn <- sum(inBin$en>bins[k+1] & noDesc)
-
-    inBin$st[inBin$st>bins[k]] <- bins[k]
-    inBin$en[inBin$en<bins[k+1]] <- bins[k+1]
-    brln <- sum(inBin$st-inBin$en)
-    rwb[k,2:7] <- c(nn, brln, orig, extn, orig/brln, extn/brln) 
-    }
-  rwb <- rwb[-nb,]
-  rwb <- rwb[rev(order(rwb$binStart)),]
+  rwb <- data.frame(binStart = bins, N = NA, branchLength = NA, orig = NA, extn = NA, lambda = NA, mu = NA)
+  for (k in 1:(nb - 1)) 
+     {
+     if(k!=1)
+       {
+       rm(orig)
+       rm(extn)
+       rm(nn)
+       rm(brln)
+       }
+     inBin <- bpp[bpp$st > bins[k + 1] & bpp$en <= bins[k], ]
+     nn <- sum(inBin$st > bins[k])
+     orig <- sum(inBin$st <= bins[k])
+     extn <- sum(inBin$en > bins[k + 1])
+     inBin$st[inBin$st > bins[k]] <- bins[k]
+     inBin$en[inBin$en < bins[k + 1]] <- bins[k + 1]
+     brln <- sum(inBin$st - inBin$en)
+     rwb[k, 2:7] <- c(nn, brln, orig, extn, orig/brln, extn/brln)
+     }
+  rwb <- rwb[-nb, ]
+  rwb <- rwb[rev(order(rwb$binStart)), ]
+  rwb$logN <- log(rwb$N)
+  rwb$growth <- c(NA, rwb$N[-(nb-1)]/rwb$N[-1])
   return(rwb)
   }
+
+
 
